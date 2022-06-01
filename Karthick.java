f1bbf80fa15dd1b3d22e89f92b69825d87e5ba0b
unknown <kkarthick878.gmail.com>
2022-06-01 20:52:47 +0530

    my files

---
+++ Karthick.java
@@ -0,0 +1,12 @@
+class Karthick
+{
+	 public static void main(String arg[])
+    {
+        System.out.println("My name is karthick, My life is my wife");
+    }
+}
+
+https://github.com/raghavendramj/MasterClassSL_23-05-2022
+https://github.com/raghavendramj/CollectionsMultiThreadingException/tree/main/CollectionsMultiThreadingException/src/com/java/learning
+
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/
